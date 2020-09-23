eb0a2ba60b6d9ad5d7b2cbd6c5a8d3f21788fd8b
atulbhalerao <atulsbhalerao07@yahoo.com>
2020-09-23 21:31:39 +0530

    Changes related to assignment 1

---
+++ src/Components/HelloWorld.js
@@ -0,0 +1,13 @@
+import { render } from '@testing-library/react'
+import React from 'react'
+
+function HelloWorld()
+{
+
+    render()
+    {
+        return <h1>Hello World</h1>
+    }
+}
+
+export default HelloWorld
